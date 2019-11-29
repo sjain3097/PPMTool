f94b38b363f1f0b2b6c1c315de6ec9b18ced056e
sankalp <sjain4688@gmail.com>
2019-11-29 15:46:52 +0530

    custom exception for already existing projectIdentifier

--- ppmtool/src/main/java/com/fullstack/ppmtool/services/ProjectService.java
+++ ppmtool/src/main/java/com/fullstack/ppmtool/services/ProjectService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.fullstack.ppmtool.domain.Project;
+import com.fullstack.ppmtool.exceptions.ProjectIdException;
 import com.fullstack.ppmtool.repositories.ProjectRepositories;
 
 @Service
@@ -12,6 +13,10 @@ public class ProjectService {
 	private ProjectRepositories projectRepository;
 	
 	public Project saveOrUpdateProject(Project project){
-		return projectRepository.save(project);
+		try{
+			return projectRepository.save(project);
+		}catch(Exception e){
+			throw new ProjectIdException("Project Id: '"+ project.getProjectIdentifier().toUpperCase()+"' already exist");
+		}
 	}
 }
